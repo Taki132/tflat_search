da369e35559736f9d309ccb0f048c839fe842a61
Karuto <nguyenthaison609@outlook.com>
2023-10-07 21:13:08 +0700

    modified:   dict.py

--- dict.py
+++ dict.py
@@ -2,7 +2,12 @@ import json
 with open ("output.json","r") as dict: 
    data = json.load(dict)
 tukhoa = input("nhập từ khóa tiếng anh cần tìm kiếm: ")
-if tukhoa in (data['entries'['part'['meaning']]]):
-    print(True)
-else:
-    print(False)
+x = data['entries']
+if tukhoa in  (data['entries']):
+   a = tukhoa['part']
+   b = x [tukhoa]
+   c = a['_']
+   if pronunciation in (tukhoa[]):
+      print(tukhoa['pronunciation'])
+   print(b['meaning'])
+   print(b)
